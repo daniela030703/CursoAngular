d1bc5023505000ec676b49d9b863f7e3bcd3e756
Daniela18 <danielachirivim@gmail.com>
2022-12-26 14:46:59 -0500

    Version1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+  nro?:number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularEjemp';
+export class AppComponent implements OnInit {
+  title = 'Mi Primer App Angular';
+  public ArregloTarjetas: Tarjeta[]=[];
+
+  ngOnInit():  void{
+
+    this.ArregloTarjetas = [
+      {titulo:'Video1', subtitulo: 'Subtitulo Video 1'},
+      {titulo:'Video2', subtitulo: 'Subtitulo Video 2'},
+      {titulo:'Video2', subtitulo: 'Subtitulo Video 2'},
+      
+    ]
+
+  }
 }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  public image:string="https://www.batiburrillo.net/wp-content/uploads/2019/07/Ampliacio%CC%81n-de-imagen-en-li%CC%81nea-sin-perder-calidad.jpg";
+  public  titulo:string="Interporlación";
+
+  ngOnInit(): void {
+    
+  }
+}
